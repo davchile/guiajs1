cea40fb6bf1d7df85c87e6d6af939bc7f574ed68
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-05-28 23:36:19 -0400

    btn example

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Contrato Arriendo
+// info1=prompt("Ingrese la direccion")
+// info2=prompt("Ingrese el numero de departamento")
+// info3=prompt("Ingrese la comuna")
+
+// const addressSpan=document.querySelector(".direccion")
+// const deptonumberSpan=document.querySelector(".numdepto")
+// const countySpan=document.querySelector(".comuna")
+
+// addressSpan.innerHTML=info1
+// deptonumberSpan.innerHTML=info2
+// countySpan.innerHTML=info3
+
+// Modificar estilos
+// parrafo = document.querySelector("p")
+// parrafo.style.fontStyle="italic"
+
+// imagenradius = document.querySelector("img")
+estilo = prompt("Ingresa el valor del border-radius deseado")
+const styleImg = document.querySelector("img")
+styleImg.style.borderRadius = estilo
